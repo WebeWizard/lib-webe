f431ac7d45a6fc3c8af7bbda14dcec29a440fdb2
WebeWizard <webewizard@gmail.com>
2019-06-25 21:58:13 -0600

    begin to use external LimitRead crate. misc formatting and error handling changes(?)

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,73 +1,109 @@
 use std::collections::HashMap;
-use std::net::TcpStream;
 use std::io::{BufRead, BufReader};
+use std::net::TcpStream;
+
+use limit_read::LimitRead;
+
+use crate::constants::{MAX_HEADER_SIZE, MAX_REQUEST_SIZE};
 
 pub struct Request {
-	pub method: String,
-	pub uri: String,
-	pub headers: HashMap<String, String>,
-    pub message_body: Option<Box<BufRead>>
+  pub size: usize,
+  pub method: String,
+  pub uri: String,
+  pub headers: HashMap<String, String>,
+  pub message_body: Option<Box<BufRead>>,
 }
 
 pub enum RequestError {
-    ReadError // error reading from stream
+  ReadError,           // error reading from stream
+  MaxHeaderSizeError,  // individual header is too large
+  MaxRequestSizeError, // request is too large
 }
 
 impl Request {
-    pub fn new (mut buf_reader: BufReader<&TcpStream>) -> Result<Request,RequestError> {
-        //read in the first line.  Split it into Method and URI
-		let mut line = String::new();
-		match buf_reader.read_line(&mut line) {
-            Ok(0) => return Err(RequestError::ReadError), // read an empty line when expecting request line
-            Ok(_) => {
-                let mut iter = line.split_whitespace();
-                match iter.next() { // get method
-                    Some(method) => {
-                        match iter.next() { // get uri
-                            Some(uri) => {
-                                // get headers
-                                let mut headers = HashMap::<String,String>::new();
-                                let lines_iter = buf_reader.lines();
-                                for line in lines_iter {
-                                    match line {
-                                        Ok(header) => {
-                                            if header == "".to_owned() {break;}
-                                            else {
-                                                // TODO: assuming header is not split across multiple lines
-                                                // even though allowed by https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.2
-                                                let mut header_iter = header.split(':');
-                                                match header_iter.next() { // get header name
-                                                    Some(header_name) => {
-                                                        match header_iter.next() { // get header value
-                                                            Some(header_value) => {
-                                                                headers.insert(header_name.to_string(), header_value.trim().to_string());
-                                                            },
-                                                            None => return Err(RequestError::ReadError) // expected header value
-                                                        }
-                                                    },
-                                                    None => return Err(RequestError::ReadError) // expected header name
-                                                }
-                                            }
-                                        },
-                                        Err(_error) => return Err(RequestError::ReadError)
-                                    }
-                                }
-                                return Ok( Request{ 
-                                    method: method.to_uppercase().to_string(),
-                                    uri: uri.to_string(),
-                                    headers: headers,
-                                    message_body: None
-                                } );
-                                // TODO: if a 'Host' header is present, the URI is just an abs_path.
-                                // TODO: Do browsers provide the root '/' or is server expected to add it?
-                            },
-                            None => return Err(RequestError::ReadError) // expected non-whitespace
+  pub fn new(mut buf_reader: BufReader<&TcpStream>) -> Result<Request, RequestError> {
+    //read in the first line.  Split it into Method and URI
+    let mut line = String::new();
+    match buf_reader.read_line_lim(&mut line, &MAX_HEADER_SIZE) {
+      Ok(0) => return Err(RequestError::ReadError), // read an empty line when expecting request line
+      Ok(uri_size) => {
+        let mut request_size: usize = uri_size;
+        let mut iter = line.split_whitespace();
+        match iter.next() {
+          // get method
+          Some(method) => {
+            match iter.next() {
+              // get uri
+              Some(uri) => {
+                // get headers
+                let mut headers = HashMap::<String, String>::new();
+                let lines_iter = buf_reader.lines_lim(MAX_HEADER_SIZE);
+                for line in lines_iter {
+                  match line {
+                    Ok(header) => {
+                      request_size += header.len() + 1; // technically we don't know the size of the line terminator, assume \n
+                      if request_size >= MAX_REQUEST_SIZE {
+                        return Err(RequestError::MaxRequestSizeError);
+                      }
+                      if header == "".to_owned() {
+                        break;
+                      } else {
+                        // TODO: assuming header is not split across multiple lines
+                        // even though allowed by https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.2
+                        let mut header_iter = header.split(':');
+                        match header_iter.next() {
+                          // get header name
+                          Some(header_name) => {
+                            match header_iter.next() {
+                              // get header value
+                              Some(header_value) => {
+                                headers
+                                  .insert(header_name.to_string(), header_value.trim().to_string());
+                              }
+                              None => return Err(RequestError::ReadError), // expected header value
+                            }
+                          }
+                          None => return Err(RequestError::ReadError), // expected header name
                         }
-                    },
-                    None => return Err(RequestError::ReadError) // expected non-whitespace
+                      }
+                    }
+                    Err(error) => {
+                      match error.kind() {
+                        std::io::ErrorKind::NotFound => {
+                          // header too large
+                          return Err(RequestError::MaxHeaderSizeError);
+                        }
+                        _ => return Err(RequestError::ReadError),
+                      }
+                    }
+                  }
                 }
-            },
-            Err(_) => return Err(RequestError::ReadError)
+                return Ok(Request {
+                  size: request_size,
+                  method: method.to_uppercase().to_string(),
+                  uri: uri.to_string(),
+                  headers: headers,
+                  message_body: None,
+                });
+                // TODO: if a 'Host' header is present, the URI is just an abs_path.
+                // TODO: Do browsers provide the root '/' or is server expected to add it?
+              }
+              None => return Err(RequestError::ReadError), // expected non-whitespace
+            }
+          }
+          None => return Err(RequestError::ReadError), // expected non-whitespace
         }
+      }
+      // catch max header/uri size
+      Err(error) => {
+        match error.kind() {
+          std::io::ErrorKind::NotFound => {
+            // header too large
+            return Err(RequestError::MaxHeaderSizeError);
+          }
+          _ => return Err(RequestError::ReadError),
+        }
+      }
     }
+  }
 }
