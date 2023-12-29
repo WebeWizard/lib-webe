01e2ec61a8c5f14879975e343536b059b2b0bab6
WebeWizard <justin.leis@webewizard.com>
2023-12-29 11:15:29 -0700

    patch some responders. update dep versions

--- webe_auth/src/http/login.rs
+++ webe_auth/src/http/login.rs
@@ -13,66 +13,68 @@ use serde_json;
 
 #[derive(Deserialize)]
 pub struct LoginForm {
-  pub email: String,
-  pub pass: String,
+    pub email: String,
+    pub pass: String,
 }
 
 pub struct LoginResponder<'w> {
-  auth_manager: &'w WebeAuth<'w>,
+    auth_manager: &'w WebeAuth<'w>,
 }
 
 impl<'w> LoginResponder<'w> {
-  pub fn new(auth_manager: &'w WebeAuth) -> LoginResponder<'w> {
-    LoginResponder {
-      auth_manager: auth_manager,
+    pub fn new(auth_manager: &'w WebeAuth) -> LoginResponder<'w> {
+        LoginResponder {
+            auth_manager: auth_manager,
+        }
     }
-  }
 }
 
 impl<'w> Responder for LoginResponder<'w> {
-  // ALWAYS RETURN Ok(200) or Err(401) TO PREVENT LEAKING API INFORMATION
-  fn build_response(
-    &self,
-    request: &mut Request,
-    _params: &Vec<(String,String)>,
-    _validation: Validation,
-  ) -> Result<Response, u16> {
-    match &mut request.message_body {
-      Some(body_reader) => {
-        match serde_json::from_reader::<_, LoginForm>(body_reader) {
-          Ok(form) => {
-            match self.auth_manager.login(&form.email, &form.pass) {
-              Ok(session) => {
-                match serde_json::to_string(&session) {
-                  Ok(body) => {
-                    let mut response = Response::new(200);
-                    response
-                      .headers
-                      .insert("Content-Type".to_owned(), "application/json".to_owned());
-                    response
-                      .headers
-                      .insert("Content-Length".to_owned(), body.len().to_string());
-                    response.message_body = Some(Box::new(Cursor::new(body)));
-                    return Ok(response);
-                  }
-                  Err(_error) => {
-                    dbg!(_error); /* fall down into 401 response */
-                  }
-                };
-              }
-              Err(_error) => {
-                dbg!(_error); /* fall down into 401 response */
-              }
+    // ALWAYS RETURN Ok(200) or Err(401) TO PREVENT LEAKING API INFORMATION
+    fn build_response(
+        &self,
+        request: &mut Request,
+        _params: &Vec<(String, String)>,
+        _validation: Validation,
+    ) -> Result<Response, u16> {
+        match &mut request.message_body {
+            Some(body_reader) => {
+                match serde_json::from_reader::<_, LoginForm>(body_reader) {
+                    Ok(form) => {
+                        match self.auth_manager.login(&form.email, &form.pass) {
+                            Ok(session) => {
+                                match serde_json::to_string(&session) {
+                                    Ok(body) => {
+                                        let mut response = Response::new(200);
+                                        response.headers.insert(
+                                            "Content-Type".to_owned(),
+                                            "application/json".to_owned(),
+                                        );
+                                        response.headers.insert(
+                                            "Content-Length".to_owned(),
+                                            body.len().to_string(),
+                                        );
+                                        response.message_body = Some(Box::new(Cursor::new(body)));
+                                        return Ok(response);
+                                    }
+                                    Err(_error) => {
+                                        dbg!(_error); /* fall down into 401 response */
+                                    }
+                                };
+                            }
+                            Err(_error) => {
+                                dbg!(_error); /* fall down into 401 response */
+                            }
+                        }
+                    }
+                    Err(_error) => {
+                        dbg!(_error); /* fall down into 401 response */
+                    }
+                }
             }
-          }
-          Err(_error) => {
-            dbg!(_error); /* fall down into 401 response */
-          }
+            None => { /* fall down into 401 response */ }
         }
-      }
-      None => { /* fall down into 401 response */ }
+        // TODO: Have common response code responses be constants
+        return Err(401);
     }
-    // TODO: Have common response code responses be constants
-    return Err(401);
-  }
 }
--- webe_auth/src/http/secure.rs
+++ webe_auth/src/http/secure.rs
@@ -2,8 +2,6 @@
 // passes the session along to the internal responder's 'validate function so that it can make
 // - extra decisions based on the session ID
 
-use std::collections::HashMap;
-
 use crate::{AuthError, AuthManager, WebeAuth};
 use webe_web::request::Request;
 use webe_web::responders::Responder;
@@ -12,58 +10,66 @@ use webe_web::status::Status;
 use webe_web::validation::{Validation, ValidationResult};
 
 pub struct SecureResponder<'w, R: Responder> {
-  auth_manager: &'w WebeAuth<'w>,
-  internal_responder: R,
+    auth_manager: &'w WebeAuth<'w>,
+    internal_responder: R,
 }
 
 impl<'w, R: Responder> SecureResponder<'w, R> {
-  pub fn new(auth_manager: &'w WebeAuth, internal_responder: R) -> SecureResponder<'w, R>
-  where
-    R: Responder,
-  {
-    SecureResponder {
-      auth_manager: auth_manager,
-      internal_responder: internal_responder,
+    pub fn new(auth_manager: &'w WebeAuth, internal_responder: R) -> SecureResponder<'w, R>
+    where
+        R: Responder,
+    {
+        SecureResponder {
+            auth_manager: auth_manager,
+            internal_responder: internal_responder,
+        }
     }
-  }
 }
 
 impl<'w, R: Responder> Responder for SecureResponder<'w, R> {
-  fn validate(
-    &self,
-    request: &Request,
-    params: &Vec<(String,String)>,
-    _validation: Validation,
-  ) -> ValidationResult {
-    // make sure session header belongs to a valid session
-    match request.headers.get("x-webe-token") {
-      Some(token) => {
-        // pass the session along to internal validation
-        match self.auth_manager.find_valid_session(token) {
-          Ok(session) => {
-            return self
-              .internal_responder
-              .validate(request, params, Some(Box::new(session)))
-          }
-          Err(error) => match error {
-            // TODO: match session error's timeout vs other (like the system clock error)
-            AuthError::SessionError(_error) => return Err(Status::from_standard_code(403)),
-            _ => return Err(Status::from_standard_code(500)),
-          },
+    fn validate(
+        &self,
+        request: &Request,
+        params: &Vec<(String, String)>,
+        _validation: Validation,
+    ) -> ValidationResult {
+        // make sure session header belongs to a valid session
+        match request.headers {
+            Some(headers) => {
+                match headers.get("x-webe-token") {
+                    Some(token) => {
+                        // pass the session along to internal validation
+                        match self.auth_manager.find_valid_session(token) {
+                            Ok(session) => {
+                                return self.internal_responder.validate(
+                                    request,
+                                    params,
+                                    Some(Box::new(session)),
+                                )
+                            }
+                            Err(error) => match error {
+                                // TODO: match session error's timeout vs other (like the system clock error)
+                                AuthError::SessionError(_error) => {
+                                    return Err(Status::from_standard_code(403))
+                                }
+                                _ => return Err(Status::from_standard_code(500)),
+                            },
+                        }
+                    }
+                    None => return Err(Status::from_standard_code(403)),
+                }
+            }
+            None => return Err(Status::from_standard_code(403)),
         }
-      }
-      None => return Err(Status::from_standard_code(403)),
     }
-  }
 
-  fn build_response(
-    &self,
-    request: &mut Request,
-    params: &HashMap<String, String>,
-    validation: Validation,
-  ) -> Result<Response, u16> {
-    self
-      .internal_responder
-      .build_response(request, params, validation)
-  }
+    fn build_response(
+        &self,
+        request: &mut Request,
+        params: &Vec<(String, String)>,
+        validation: Validation,
+    ) -> Result<Response, u16> {
+        self.internal_responder
+            .build_response(request, params, validation)
+    }
 }
